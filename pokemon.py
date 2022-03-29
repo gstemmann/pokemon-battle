1ac058edd40fe3ca6e7fb6fb1220710ffa3ae07b
George <george.stemmann@gmail.com>
2022-03-29 00:46:07 -0700

    install pokepy

--- pokemon.py
+++ pokemon.py
@@ -1,40 +1,44 @@
 from unicodedata import name
 import requests
+import pypokedex
 
-
-
-def get_pokemon_names():
-        # """Call Pokemon api: """
-    res = requests.get(f"https://pokeapi.co/api/v2/pokemon/{i}/names")
+def get_pokemon_names(): 
+    res = requests.get("https://pokeapi.co/api/v2/pokemon")
     data = res.json()
     results = data['results']
-    print(results)
-
-
-
-    metapod = results[10]['name']
     
     for dict in results:
-        for val in dict:
             print (dict['name'])
 
+def get_pokemon_ids():
+    new_list = []
+    i = 0
+    while(i < len(new_list)): 
+        res = requests.get(f"https://pokeapi.co/api/v2/pokemon{i}")
+        data = res.json()
+        results = data['results']
+        print(results[i], end = '')
 
-    # for result in results:
-    #     pokemon = Pokemon(name=result['name'])
-    #     db.session.add()
+def get_pokemon_abilities():
+    res = requests.get("https://pokeapi.co/api/v2/pokemon/clefairy")
+    data = res.json()
+    list_of_dicts = (data['abilities'])
+    for x in list_of_dicts:
+        for dict in x.values():
+            print(dict)
+
+def get_pokemon_type():
+    res = requests.get("https://pokeapi.co/api/v2/pokemon/clefairy")
+    data = res.json()
+    list_of_dicts = (data['types'])
+    print(list_of_dicts[0])
 
 
-    # for dict in len(results):
-    #     for val in dict:
-    #         print(dict[val])
+def get_pokemon():
+    i=1
+    pokemon = []
+    while(i < len(pokemon)):
+        abilities = pypokedex.get(abilities=f"{i}")
+        print(abilities)
 
-    # for names in results['name']:
-    #     print(names)
-
-# the variable "results" is a list so must use indeces to access items
-    # for x in names:
-    #     print(x)
-    # return x
-
-
-    
+p = pypokedex.get(name='charmander')
