8442fff7a37dd9d7132b65ad0a715b71df9fd575
George <george.stemmann@gmail.com>
2022-04-13 16:33:21 -0700

    changed procfile

--- app.py
+++ app.py
@@ -1,28 +1,27 @@
 import os
-from flask import Flask, render_template, request, flash, redirect
+from flask import Flask, render_template, request, redirect
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, User
 
 
 
-app = Flask (__name__)
+
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = (
 #     os.environ.get('DATABASE_URL', 'postgresql:///pokemon'))
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI').replace('postgres://', 'postgresql://')
+
+
+app = Flask (__name__)
 
 uri = os.environ.get("DATABASE_URL", "postgresql:///pokemon")
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = uri
-
-
-
-
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI').replace('postgres://', 'postgresql://')
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'nevertell')
 toolbar = DebugToolbarExtension(app)
 
--- pokemon.py
+++ pokemon.py
@@ -1,61 +1,61 @@
-from unicodedata import name
-import requests
-import pypokedex
+# from unicodedata import name
+# import requests
+# import pypokedex
 
-def get_pokemon_names(): 
-    res = requests.get("https://pokeapi.co/api/v2/pokemon")
-    data = res.json()
-    results = data['results']
+# def get_pokemon_names(): 
+#     res = requests.get("https://pokeapi.co/api/v2/pokemon")
+#     data = res.json()
+#     results = data['results']
     
-    for dict in results:
-            print (dict['name'])
+#     for dict in results:
+#             print (dict['name'])
 
-def get_pokemon_ids():
-    new_list = []
-    i = 0
-    while(i < len(new_list)): 
-        res = requests.get(f"https://pokeapi.co/api/v2/pokemon{i}")
-        data = res.json()
-        results = data['results']
-        print(results[i], end = '')
+# def get_pokemon_ids():
+#     new_list = []
+#     i = 0
+#     while(i < len(new_list)): 
+#         res = requests.get(f"https://pokeapi.co/api/v2/pokemon{i}")
+#         data = res.json()
+#         results = data['results']
+#         print(results[i], end = '')
 
-def get_pokemon_abilities():
-    res = requests.get("https://pokeapi.co/api/v2/pokemon/clefairy")
-    data = res.json()
-    list_of_dicts = (data['abilities'])
-    for x in list_of_dicts:
-        for dict in x.values():
-            print(dict)
+# def get_pokemon_abilities():
+#     res = requests.get("https://pokeapi.co/api/v2/pokemon/clefairy")
+#     data = res.json()
+#     list_of_dicts = (data['abilities'])
+#     for x in list_of_dicts:
+#         for dict in x.values():
+#             print(dict)
 
-def get_pokemon_type():
-    res = requests.get("https://pokeapi.co/api/v2/pokemon/clefairy")
-    data = res.json()
-    list_of_dicts = (data['types'])
-    print(list_of_dicts[0])
-
-
-def get_pokemon():
-    i=1
-    pokemon = []
-    while(i < len(pokemon)):
-        abilities = pypokedex.get(abilities=f"{i}")
-        print(abilities)
+# def get_pokemon_type():
+#     res = requests.get("https://pokeapi.co/api/v2/pokemon/clefairy")
+#     data = res.json()
+#     list_of_dicts = (data['types'])
+#     print(list_of_dicts[0])
 
 
+# def get_pokemon():
+#     i=1
+#     pokemon = []
+#     while(i < len(pokemon)):
+#         abilities = pypokedex.get(abilities=f"{i}")
+#         print(abilities)
 
 
 
 
 
-def get_data():
-    list = []
-    for pokemon in list:
-        pypokedex.get(name=pokemon)
-        list.push(pokemon.name)
-        print()
 
-get_data()
-# @app.route('/pokemon/show')
+
+# def get_data():
+#     list = []
+#     for pokemon in list:
+#         pypokedex.get(name=pokemon)
+#         list.push(pokemon.name)
+#         print()
+
+# get_data()
+# # @app.route('/pokemon/show')
 # def homepage():
 #     """Show homepage: """
 
